dd1b06d3b32ad22779e3232f1e4ee32842706ebb
Renato Fuzaro Miotto <renato.fmiotto@gmail.com>
2022-10-21 13:05:37 -0300

    feat: load and save checkpoints

---
+++ src/checkpoint.py
@@ -0,0 +1,30 @@
+import torch
+
+
+def save_checkpoint(
+    model, optimizer, epoch, loss, best_acc, filename="my_checkpoint.pth.tar"
+):
+    print("=> Saving checkpoint")
+
+    state = {
+        "model_state_dict": model.state_dict(),
+        "optimizer_state_dict": optimizer.state_dict(),
+        "epoch": epoch,
+        "loss": loss,
+        "best_acc": best_acc,
+    }
+
+    torch.save(state, filename)
+
+
+def load_checkpoint(device, model, optimizer, filename="my_checkpoint.pth.tar"):
+    print("=> Loading checkpoint")
+
+    checkpoint = torch.load(filename, map_location=device)
+    model.load_state_dict(checkpoint["model_state_dict"])
+    optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
+    epoch = checkpoint["epoch"]
+    loss = checkpoint["loss"]
+    best_acc = checkpoint["best_acc"]
+
+    return epoch, loss, best_acc
