9ab450b81d3dcbbc2c0707a62e88d68ef84d3bfd
Renato Fuzaro Miotto <renato.fmiotto@gmail.com>
2022-11-10 18:53:50 -0300

    fix: print duration of training

--- src/timeit.py
+++ src/timeit.py
@@ -1,20 +1,30 @@
 from functools import wraps
-import time
+from datetime import datetime
+
+
+def date_diff_in_seconds(dt2, dt1):
+    timedelta = dt2 - dt1
+    return timedelta.days * 24 * 3600 + timedelta.seconds
+
+
+def dhms_from_seconds(seconds):
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    return days, hours, minutes, seconds
 
 
 def timeit(func):
     @wraps(func)
     def timeit_wrapper(*args, **kwargs):
-        start_time = time.perf_counter()
+        start_time = datetime.now()
         result = func(*args, **kwargs)
-        end_time = time.perf_counter()
-        elapsed_time = end_time - start_time
+        end_time = datetime.now()
+        elapsed_time = date_diff_in_seconds(end_time, start_time)
+        days, hours, minutes, seconds = dhms_from_seconds(elapsed_time)
         print(
             "Training complete in "
-            + f"{elapsed_time // (60 * 60 * 24):.0f}d  "
-            + f"{elapsed_time // (60 * 60):.0f}h  "
-            + f"{elapsed_time // 60:.0f}m  "
-            + f"{elapsed_time % 60:.0f}s"
+            + f"{days} days, {hours} hours, {minutes} minutes, {seconds} seconds"
         )
         return result
 
